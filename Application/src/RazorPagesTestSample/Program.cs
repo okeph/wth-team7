a5958b5678c6912cf90a662527afad66c0a3301a
WillOShea <45031991+WillOShea@users.noreply.github.com>
2024-03-21 14:29:18 +0000

    Remove unnecessary comment in Main method

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -12,7 +12,6 @@ namespace RazorPagesTestSample
     {
         public static void Main(string[] args)
         {
-            // Comment added
             var host = CreateHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
