4348aaad26f2e84389ae9a0f62872d71be613cf1
WillOShea <45031991+WillOShea@users.noreply.github.com>
2024-03-21 14:43:39 +0000

    Break  semicolon in Program.cs

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using RazorPagesTestSample.Data;
+using RazorPagesTestSample.Data
 
 namespace RazorPagesTestSample
 {
