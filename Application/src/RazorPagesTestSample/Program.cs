af5c68a88985b1a79db677ff5ad4a579ec4ae1d9
WillOShea <45031991+WillOShea@users.noreply.github.com>
2024-03-21 14:33:40 +0000

    Add  unnecessary comment in Program.cs

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -12,6 +12,7 @@ namespace RazorPagesTestSample
     {
         public static void Main(string[] args)
         {
+            // Unnecessary comment
             var host = CreateHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
